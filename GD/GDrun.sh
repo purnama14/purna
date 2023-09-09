02f0e0f46e0d0bfa567becb25559b254960f4d91
purnama14 <138311687+purnama14@users.noreply.github.com>
2023-09-10 02:57:06 +0700

    Update GDrun.sh

--- GD/GDrun.sh
+++ GD/GDrun.sh
@@ -6,7 +6,7 @@ rm -rf /home/chrome60/Default/Cache/*
 rm -rf /root/Downloads/*
 sleep 2
  google-chrome --user-data-dir=/home/chrome60 --start-maximized --no-sandbox --incognito & sleep 5
- google-chrome --user-data-dir=/home/chrome60 https://kotrigames.com.com/go.php --start-maximized --no-sandbox --incognito & sleep 1
+ google-chrome --user-data-dir=/home/chrome60 https://kotrigames.com/go.php --start-maximized --no-sandbox --incognito & sleep 1
 clear
 sleep 10
 clear
