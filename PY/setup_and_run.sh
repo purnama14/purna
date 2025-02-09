bf19ba6609deeb0e3c26e0e17bad397675a056ab
purnama14 <138311687+purnama14@users.noreply.github.com>
2025-02-09 18:21:24 +0700

    Update setup_and_run.sh

--- PY/setup_and_run.sh
+++ PY/setup_and_run.sh
@@ -5,7 +5,8 @@ git clone https://github.com/purnama14/purna/raw/refs/heads/main/PY/helpbot.py
 cd helpbot
 
 # Instal dependencies
-pip install -r https://github.com/purnama14/purna/raw/refs/heads/main/PY/requirements.txt
+wget https://github.com/purnama14/purna/raw/refs/heads/main/PY/requirements.txt
+pip install -r requirements.txt
 
 # Instal WebDriver
 sudo apt install -y chromium-browser chromium-chromedriver firefox geckodriver
