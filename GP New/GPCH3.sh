8f39344e715e89100325dd852690d7dbf7dae148
purnama14 <138311687+purnama14@users.noreply.github.com>
2025-04-03 03:10:06 +0700

    Update GPCH3.sh

--- GP New/GPCH3.sh
+++ GP New/GPCH3.sh
@@ -9,41 +9,47 @@ random_zip=${zip_files[RANDOM % ${#zip_files[@]}]}
 echo "Extracting file: $random_zip"
 unzip -o "$random_zip"
 clear
- google-chrome --user-data-dir=/home/chrome60 https://mazbron.net/close.html --window-size=1900,1200 --window-position=343,0 --no-sandbox  & sleep 5
+ google-chrome --user-data-dir=/home/chrome60 https://mazbron.net/close.php --force-device-scale-factor=0.7 --window-size=1900,1200 --window-position=343,0 --no-sandbox  & sleep 5
 
- google-chrome --user-data-dir=/home/chrome60 https://bobabobi.com --no-sandbox  & termdown 5
-xdotool mousemove 933 403 click 1 & sleep 1
-xdotool mousemove 1629 406 click 1 
+ google-chrome --user-data-dir=/home/chrome60 https://playwddya.com --no-sandbox  & termdown 5
+xdotool mousemove 393 289 click 1 & sleep 1
+xdotool mousemove 884 286 click 1 
 termdown 5
- google-chrome --user-data-dir=/home/chrome60 https://bobabobi.com --no-sandbox  & termdown 5
-xdotool mousemove 933 403 click 1 & sleep 1
-xdotool mousemove 1629 406 click 1 
+ google-chrome --user-data-dir=/home/chrome60 https://playwddya.com --no-sandbox  & termdown 5
+xdotool mousemove 393 289 click 1 & sleep 1
+xdotool mousemove 884 286 click 1  
 termdown 5
- google-chrome --user-data-dir=/home/chrome60 https://bobabobi.com --no-sandbox  & termdown 5
-xdotool mousemove 933 403 click 1 & sleep 1
-xdotool mousemove 1629 406 click 1 
+ google-chrome --user-data-dir=/home/chrome60 https://playwddya.com --no-sandbox  & termdown 5
+xdotool mousemove 393 289 click 1 & sleep 1
+xdotool mousemove 884 286 click 1  
 termdown 5
- google-chrome --user-data-dir=/home/chrome60 https://bobabobi.com --no-sandbox  & termdown 5
-xdotool mousemove 933 403 click 1 & sleep 1
-xdotool mousemove 1629 406 click 1  
+ google-chrome --user-data-dir=/home/chrome60 https://playwddya.com --no-sandbox  & termdown 5
+xdotool mousemove 393 289 click 1 & sleep 1
+xdotool mousemove 884 286 click 1  
 termdown 5
- google-chrome --user-data-dir=/home/chrome60 https://bobabobi.com/go.php --no-sandbox  & termdown 5
-xdotool mousemove 933 403 click 1 & sleep 1
-xdotool mousemove 1629 406 click 1   
+ google-chrome --user-data-dir=/home/chrome60 https://playwddya.com --no-sandbox  & termdown 5
+xdotool mousemove 393 289 click 1 & sleep 1
+xdotool mousemove 884 286 click 1   
 termdown 5
 
-xdotool mousemove 507 22 click 1 & sleep 2
-xdotool mousemove 739 22 click 1 & sleep 2
-xdotool mousemove 974 21 click 1 & sleep 2
-xdotool mousemove 1233 21 click 1 & sleep 2
-xdotool mousemove 1444 20 click 1 & sleep 2
+xdotool mousemove 110 13 click 1 & sleep 2
+xdotool mousemove 288 13 click 1 & sleep 2
+xdotool mousemove 412 13 click 1 & sleep 2
+xdotool mousemove 578 13 click 1 & sleep 2
+xdotool mousemove 764 13 click 1 & sleep 2
 
-xdotool mousemove 507 22 click 1 & sleep 2
-xdotool mousemove 739 22 click 1 & sleep 2
-xdotool mousemove 974 21 click 1 & sleep 2
-xdotool mousemove 1233 21 click 1 & sleep 2
-xdotool mousemove 1444 20 click 1 & sleep 2
+xdotool mousemove 110 13 click 1 & sleep 2
+xdotool mousemove 288 13 click 1 & sleep 2
+xdotool mousemove 412 13 click 1 & sleep 2
+xdotool mousemove 578 13 click 1 & sleep 2
+xdotool mousemove 764 13 click 1 & sleep 2
 
+
+xdotool mousemove 110 13 click 1 & sleep 2
+xdotool mousemove 288 13 click 1 & sleep 2
+xdotool mousemove 412 13 click 1 & sleep 2
+xdotool mousemove 578 13 click 1 & sleep 2
+xdotool mousemove 764 13 click 1 & sleep 2
 
 
 
