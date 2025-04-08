7ae4e7bfbe50908f6f06c1f5498c7c89728d48a3
purnama14 <138311687+purnama14@users.noreply.github.com>
2025-04-09 02:48:32 +0700

    Create RUNNITRO.sh

---
+++ Nitro/RUNNITRO.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+printf '\033]2;helpbot\a'
+while :
+do
+rm -rf /home/chrome60 & termdown 3
+cd /home
+zip_files=(*.zip)
+random_zip=${zip_files[RANDOM % ${#zip_files[@]}]}
+echo "Extracting file: $random_zip"
+unzip -o "$random_zip"
+clear
+ google-chrome --user-data-dir=/home/chrome60 https://mazbron.net/close.php --force-device-scale-factor=0.7 --window-size=1900,1200 --window-position=343,0 --start-maximized --no-sandbox --incognito & sleep 5
+
+ google-chrome --user-data-dir=/home/chrome60 https://sasabhsu.com/go.php --start-maximized --no-sandbox --incognito & termdown 5
+xdotool mousemove 1563 544 click 1 & sleep 1 
+termdown 5
+ google-chrome --user-data-dir=/home/chrome60 https://sasabhsu.com/go.php --start-maximized --no-sandbox --incognito & termdown 5
+xdotool mousemove 1563 544 click 1 & sleep 1 
+termdown 5 
+ google-chrome --user-data-dir=/home/chrome60 https://sasabhsu.com/go.php --start-maximized --no-sandbox --incognito & termdown 5
+xdotool mousemove 1563 544 click 1 & sleep 1 
+termdown 5
+
+xdotool mousemove 343 13 click 1 & sleep 2
+xdotool mousemove 496 13 click 1 & sleep 2
+xdotool mousemove 671 13 click 1 & sleep 2
+
+xdotool mousemove 343 13 click 1 & sleep 2
+xdotool mousemove 496 13 click 1 & sleep 2
+xdotool mousemove 671 13 click 1 & sleep 2
+
+xdotool mousemove 343 13 click 1 & sleep 2
+xdotool mousemove 496 13 click 1 & sleep 2
+xdotool mousemove 671 13 click 1 & sleep 2
+
+xdotool mousemove 343 13 click 1 & sleep 2
+xdotool mousemove 496 13 click 1 & sleep 2
+xdotool mousemove 671 13 click 1 & sleep 2
+
+pkill --oldest chrome
+pkill --oldest chrome
+pkill --oldest chrome
+sudo apt autoclean
+termdown 3
+done
