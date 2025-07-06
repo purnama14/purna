cbef754823d80d1e05fa742af7eb9824f74aa516
purnama14 <138311687+purnama14@users.noreply.github.com>
2025-07-07 03:48:11 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,112 +2,110 @@
 set -e
 export DEBIAN_FRONTEND=noninteractive
 
-echo "======================================="
-echo "   ANANG INSTALLER FOR UBUNTU 20.04"
-echo "======================================="
+echo "========================================="
+echo " Installing figlet & lolcat for banner..."
+echo "========================================="
+apt update -y
+apt install -y figlet ruby
+gem install lolcat || true
 
-# AUTO KEEP LOCAL CONFIG FILES
+clear
+figlet -f big ANANG | lolcat
+sleep 2
+
 echo 'Dpkg::Options {
    "--force-confdef";
    "--force-confold";
 };' > /etc/apt/apt.conf.d/99noconfprompt
 
-# UPDATE & BASIC
-apt update -y && apt upgrade -y
+echo -e "\e[1;32mUpdating system...\e[0m"
+apt update -y
+apt upgrade -y
 
-echo "Installing utilities..."
-apt install -y zip unzip nano wget curl lsb-release software-properties-common
+echo -e "\e[1;36mInstalling basic tools...\e[0m"
+apt install -y zip unzip nano curl wget lsb-release software-properties-common
 
-# JAVA & BROWSERS
-echo "Installing Java..."
-apt install -y openjdk-8-jdk
+echo -e "\e[1;35mInstalling actiona, openjdk, firefox...\e[0m"
+apt install -y actiona openjdk-8-jdk firefox
 
-echo "Installing Firefox..."
-apt install -y firefox
-
-echo "Installing Google Chrome..."
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
-echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
+echo -e "\e[1;34mInstalling Google Chrome...\e[0m"
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | gpg --dearmor -o /usr/share/keyrings/google-chrome.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main" \
+    | tee /etc/apt/sources.list.d/google-chrome.list
 apt update -y
 apt install -y google-chrome-stable
 
-# ACTIONA
-echo "Installing Actiona..."
-apt install -y actiona
+echo -e "\e[1;33mInstalling flash plugin...\e[0m"
+apt install -y flashplugin-installer
+wget https://github.com/purnama14/purna/raw/main/install_flash_player_11_linux.x86_64.tar.gz
+tar -xvf install_flash_player_11_linux.x86_64.tar.gz
+cp libflashplayer.so /usr/lib/mozilla/plugins
 
-# XRDP & LXDE
-echo "Installing XRDP & LXDE..."
-apt install -y xrdp lxde net-tools xdotool
-adduser xrdp ssl-cert || true
-systemctl restart xrdp
-systemctl enable xrdp
-ufw allow 3389
-ufw allow from 1.1.1.1 to any port 3389
+echo -e "\e[1;32mDownloading chrome profiles & setups...\e[0m"
+cd /home
+wget kizegame.com/BH/chrome2.zip
+unzip chrome2.zip
 
-# APACHE2
-echo "Installing Apache2..."
-apt install -y apache2
+mkdir nl && cd nl
+wget kizegame.com/BH/NL.tar.gz
+tar -xvf NL.tar.gz
+cd /home
+mkdir de && cd de
+wget kizegame.com/BH/DE.tar.gz
+tar -xvf DE.tar.gz
 
-# PROXYCHAINS & SL
-echo "Installing Proxychains & sl..."
-apt install -y proxychains sl
+cd
+mkdir -p Desktop
+cd Desktop
+wget https://github.com/purnama14/purna/raw/main/chr.sh
+wget https://github.com/purnama14/purna/raw/main/GD/FI.ascr
+wget https://github.com/purnama14/purna/raw/main/GD/GDrun.sh
+wget https://github.com/purnama14/purna/raw/main/GD/link.sh
+wget https://github.com/purnama14/purna/raw/main/GD/play.png
+wget https://github.com/purnama14/purna/raw/main/GD/kontri.png
+wget https://github.com/purnama14/purna/raw/refs/heads/main/GD/XDrun.sh
+wget https://github.com/purnama14/purna/raw/main/gp.ascr
+wget https://github.com/purnama14/purna/raw/main/gpclick.ascr
+wget https://github.com/purnama14/purna/raw/main/run.sh
+wget https://github.com/purnama14/purna/raw/refs/heads/main/GP%20New/PC/GPCH3.sh
+wget https://github.com/purnama14/purna/raw/refs/heads/main/Nitro/RUNNITRO.sh
+chmod +x *.sh *.ascr
 
-# TERM DOWN
-echo "Installing termdown..."
+echo "Setting root password..."
+echo "root:KiZeg4me2@fa" | chpasswd
+
+echo "Installing apache2, proxychains, sl, xdotool, termdown..."
+apt install -y apache2 proxychains sl xdotool
 snap install termdown
-pip install termdown
 
-# CONFIG HOSTS & PROXYCHAINS
-echo "Updating /etc/hosts and proxychains.conf..."
+echo "Updating /etc/hosts & proxychains.conf..."
 cd /etc
 mv hosts hosts.bak || true
 mv proxychains.conf proxychains.conf.bak || true
-wget -q https://github.com/purnama14/purna/raw/main/fmb/proxychains.conf
-wget -q https://github.com/purnama14/purna/raw/main/hosts
+wget https://github.com/purnama14/purna/raw/main/fmb/proxychains.conf
+wget https://github.com/purnama14/purna/raw/main/hosts
 cp hosts hosts.bak
 
-# REMOVE SCREENSAVER
-echo "Removing xscreensaver..."
+pip install termdown || true
+
+echo "Installing XRDP & LXDE..."
+apt install -y xrdp lxde net-tools
+adduser xrdp ssl-cert || true
+systemctl restart xrdp
+systemctl enable xrdp
+
+echo "Configuring firewall..."
+ufw allow 3389
+ufw allow from 1.1.1.1 to any port 3389
+
+echo "Removing screensaver..."
 apt remove -y xscreensaver
 
-# INSTALL FILES & SCRIPTS
-echo "Downloading your Chrome profiles & scripts..."
-cd /home
-wget -q kizegame.com/BH/chrome2.zip && unzip chrome2.zip
+echo "Cleaning up..."
+apt autoclean -y
+apt-get -y --fix-broken install
 
-mkdir -p nl && cd nl
-wget -q kizegame.com/BH/NL.tar.gz && tar -xvf NL.tar.gz
-
-cd /home
-mkdir -p de && cd de
-wget -q kizegame.com/BH/DE.tar.gz && tar -xvf DE.tar.gz
-
-cd && mkdir -p Desktop && cd Desktop
-wget -q https://github.com/purnama14/purna/raw/main/chr.sh
-wget -q https://github.com/purnama14/purna/raw/main/GD/FI.ascr
-wget -q https://github.com/purnama14/purna/raw/main/GD/GDrun.sh
-wget -q https://github.com/purnama14/purna/raw/main/GD/link.sh
-wget -q https://github.com/purnama14/purna/raw/main/GD/play.png
-wget -q https://github.com/purnama14/purna/raw/main/GD/kontri.png
-wget -q https://github.com/purnama14/purna/raw/refs/heads/main/GD/XDrun.sh
-wget -q https://github.com/purnama14/purna/raw/main/gp.ascr
-wget -q https://github.com/purnama14/purna/raw/main/gpclick.ascr
-wget -q https://github.com/purnama14/purna/raw/main/run.sh
-wget -q https://github.com/purnama14/purna/raw/refs/heads/main/GP%20New/PC/GPCH3.sh
-wget -q https://github.com/purnama14/purna/raw/refs/heads/main/Nitro/RUNNITRO.sh
-
-chmod +x *.sh *.ascr
-
-# SET PASSWORD ROOT
-echo "root:KiZeg4me2@fa" | chpasswd
-
-# RESTART SERVICES
-echo "Restarting services..."
-systemctl restart xrdp
-service apache2 restart
-
-echo "======================================="
-echo " LXDE Desktop & XRDP installed on Ubuntu 20.04"
-echo " Login RDP with root / KiZeg4me2@fa"
-echo " Cheers from ANANG INSTALLER 🚀"
-echo "======================================="
+echo -e "\e[1;36m==============================================\e[0m"
+echo -e "\e[1;32mYou have successfully installed ANANG system!\e[0m"
+echo -e "\e[1;33mLXDE + XRDP ready. Enjoy your VPS!\e[0m"
+echo -e "\e[1;36mCeeeers... MAZBRON.com _ BESTSEOTOOL.co\e[0m"
