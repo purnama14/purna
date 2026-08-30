761a70d2b0d2e2d4754009756ec06837458d954f
purnama14 <138311687+purnama14@users.noreply.github.com>
2026-08-30 08:57:17 +0700

    Update install.sh

--- GD/install.sh
+++ GD/install.sh
@@ -109,14 +109,14 @@ chmod +x chr.sh
 #wget https://github.com/purnama14/purna/raw/refs/heads/main/GP%20New/PC/GPCH3.sh
 wget https://github.com/purnama14/purna/raw/refs/heads/main/Nitro/RUNNITRO.sh
 wget https://github.com/purnama14/purna/raw/refs/heads/main/PGRUN.sh
-wget https://github.com/purnama14/purna/raw/refs/heads/main/RUNGD.sh
+wget https://github.com/purnama14/purna/raw/refs/heads/main/PGLEPRUN.sh
 #chmod +x gp.ascr
 #chmod +x gpclick.ascr
 #chmod +x run.sh
 #chmod +x GPCH3.sh
 chmod +x RUNNITRO.sh
 chmod +x PGRUN.sh
-chmod +x RUNGD.sh
+chmod +x PGLEPRUN.sh
 #wget https://github.com/purnama14/purna/raw/main/GP%20New/FIGP.ascr
 #wget https://github.com/purnama14/purna/raw/main/GP%20New/GPNew.sh
 #wget https://github.com/purnama14/purna/raw/main/GP%20New/linkFIGP.sh
