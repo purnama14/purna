52cc42c56d5db06d14ce9397c742500b51fda0a3
purnama14 <138311687+purnama14@users.noreply.github.com>
2026-08-30 10:03:59 +0700

    Update RUNNITRO.sh

--- Nitro/RUNNITRO.sh
+++ Nitro/RUNNITRO.sh
@@ -9,39 +9,31 @@ random_zip=${zip_files[RANDOM % ${#zip_files[@]}]}
 echo "Extracting file: $random_zip"
 unzip -o "$random_zip"
 clear
- google-chrome --user-data-dir=/home/chrome60 https://mazbron.net/close.php --force-device-scale-factor=0.7 --window-size=1900,1200 --window-position=343,0 --start-maximized --no-sandbox --incognito & sleep 5
+ google-chrome --user-data-dir=/home/chrome60 https://gamehtml5.com/close.php --force-device-scale-factor=0.7 --window-size=1900,1200 --window-position=343,0 --start-maximized --no-sandbox --incognito & sleep 5
 
- google-chrome --user-data-dir=/home/chrome60 https://sasabhsu.com/go.php --start-maximized --no-sandbox --incognito & termdown 5
-xdotool mousemove 1563 544 click 1 & sleep 1 
+ google-chrome --user-data-dir=/home/chrome60 https://sasabhsu.com --start-maximized --no-sandbox --incognito & termdown 5
 termdown 5
- google-chrome --user-data-dir=/home/chrome60 https://sasabhsu.com/go.php --start-maximized --no-sandbox --incognito & termdown 7
-xdotool mousemove 1563 544 click 1 & sleep 1 
-xdotool mousemove 1208 233 click 1 & sleep 1 
-
-termdown 5 
- google-chrome --user-data-dir=/home/chrome60 https://sasabhsu.com/go.php --start-maximized --no-sandbox --incognito & termdown 5
-xdotool mousemove 1563 544 click 1 & sleep 1 
+ google-chrome --user-data-dir=/home/chrome60 https://sasabhsu.com --start-maximized --no-sandbox --incognito & termdown 5
+termdown 5
+ google-chrome --user-data-dir=/home/chrome60 https://sasabhsu.com --start-maximized --no-sandbox --incognito & termdown 5
+termdown 5
+ google-chrome --user-data-dir=/home/chrome60 https://sasabhsu.com --start-maximized --no-sandbox --incognito & termdown 5
 termdown 5
 
-xdotool mousemove 343 13 click 1 & sleep 2
-xdotool mousemove 496 13 click 1 & sleep 2
-xdotool mousemove 671 13 click 1 & sleep 2
-xdotool mousemove 854 13 click 1 & sleep 2
+xdotool mousemove 97 15 click 1 & sleep 3
+xdotool mousemove 243 15 click 1 & sleep 3
+xdotool mousemove 419 15 click 1 & sleep 3
+xdotool mousemove 588 15 click 1 & sleep 3
 
-xdotool mousemove 343 13 click 1 & sleep 2
-xdotool mousemove 496 13 click 1 & sleep 2
-xdotool mousemove 671 13 click 1 & sleep 2
-xdotool mousemove 854 13 click 1 & sleep 2
+xdotool mousemove 97 15 click 1 & sleep 3
+xdotool mousemove 243 15 click 1 & sleep 3
+xdotool mousemove 419 15 click 1 & sleep 3
+xdotool mousemove 588 15 click 1 & sleep 3
 
-xdotool mousemove 343 13 click 1 & sleep 2
-xdotool mousemove 496 13 click 1 & sleep 2
-xdotool mousemove 671 13 click 1 & sleep 2
-xdotool mousemove 854 13 click 1 & sleep 2
-
-xdotool mousemove 343 13 click 1 & sleep 2
-xdotool mousemove 496 13 click 1 & sleep 2
-xdotool mousemove 671 13 click 1 & sleep 2
-xdotool mousemove 854 13 click 1 & sleep 2
+xdotool mousemove 97 15 click 1 & sleep 3
+xdotool mousemove 243 15 click 1 & sleep 3
+xdotool mousemove 419 15 click 1 & sleep 3
+xdotool mousemove 588 15 click 1 & sleep 3
 
 pkill --oldest chrome
 pkill --oldest chrome
